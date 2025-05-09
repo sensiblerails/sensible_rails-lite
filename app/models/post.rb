97db47c3595045d6d5ff61eeb0a624b83d645159
davidteren <drteren@gmail.com>
2025-05-10 01:47:39 +0200

    Implement real-time updates for posts using SolidCable and add system tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,8 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
-  broadcasts_to ->(post) { "posts" }, inserts_by: :prepend
+  # Fix broadcasting to ensure it works with SolidCable
+  after_create_commit -> { broadcast_prepend_to "posts", target: "posts", partial: "posts/post", locals: { post: self } }
+  after_update_commit -> { broadcast_replace_to "posts", target: "post_#{id}", partial: "posts/post", locals: { post: self } }
+  after_destroy_commit -> { broadcast_remove_to "posts", target: "post_#{id}" }
 end
